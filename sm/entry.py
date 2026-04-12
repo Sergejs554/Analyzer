71eb4f921540283d38d3a8427b556acb406908e9
Sergejs554 <112377059+Sergejs554@users.noreply.github.com>
2026-04-12 21:59:36 +0300

    Add parameters for neutral preclean and AFFTDN

--- sm/entry.py
+++ sm/entry.py
@@ -9,6 +9,8 @@ def render_sm_branch_v1(
     intensity: str,
     fmt: str,
     td: str,
+    use_neutral_preclean: bool = True,
+    enable_afftdn: bool = False,
 ):
     return render_sm_core_v1(
         input_path=input_path,
@@ -16,5 +18,6 @@ def render_sm_branch_v1(
         intensity=intensity,
         fmt=fmt,
         td=td,
-        use_neutral_preclean=True,
+        use_neutral_preclean=use_neutral_preclean,
+        enable_afftdn=enable_afftdn,
     )
