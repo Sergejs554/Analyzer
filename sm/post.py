b117f116b6b38c92c781f9195f992569cd4f3a4a
Sergejs554 <112377059+Sergejs554@users.noreply.github.com>
2026-04-12 21:24:11 +0300

    Create render_sm_post_stage function stub
    Add initial implementation of render_sm_post_stage function.

---
+++ sm/post.py
@@ -0,0 +1,8 @@
+# sm/post.py
+
+def render_sm_post_stage(input_path: str, fmt: str, td: str):
+    """
+    Сюда потом переносим текущий _render_post_stage
+    и two-pass loudnorm.
+    """
+    raise NotImplementedError("render_sm_post_stage is not implemented yet")
